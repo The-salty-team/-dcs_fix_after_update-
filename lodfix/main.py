e6ff30af9a39419514841b1689738a6a0b6848f8
Körtvélyes Patrik <91703430+Saltybusdriver@users.noreply.github.com>
2022-07-12 16:42:14 +0200

    fixed ting bruv

--- lodfix/main.py
+++ lodfix/main.py
@@ -4,36 +4,30 @@ import os
 
 class readFile:
 
-    currdir=os.path.dirname(__file__)
-    subpath="Config/graphics.lua"
-    file_path=os.path.join(currdir,subpath)
-    Flist = open("graphics.lua", "r+")
+    currdir = os.path.dirname(__file__)
+    subpath = "Config/graphics.lua"
+    file_path = os.path.join(currdir,subpath)
     linenum = 0
     data = []
 
     def read(self):
+        self.Flist = open(self.file_path, "r+")
         self.data = self.Flist.readlines()
         for x in self.data:
-            self.linenum=self.linenum+1
+            self.linenum = self.linenum+1
             if "around_camera" in x:
                 print(self.linenum)
                 break
-        self.data[self.linenum-1] ="around_camera = 100;"
-        self.data[self.linenum] = "around_objects = 1000;"
+        self.data[self.linenum-1] = "\taround_camera = 100;\n"
+        self.data[self.linenum] = "\taround_objects = 1000;\n"
         self.Flist.close()
 
 
-
-
-    def write(self,asdf):
-        self.Flist=open("graphics.lua", "w")
+    def write(self):
+        self.Flist = open(self.file_path, "w+")
         self.Flist.writelines(self.data)
-    def getId(self):
-        return self.TweetData
-
 
 
 f = readFile()
 f.read()
-print(f.data[13])
-f.write("asdf")
+f.write()
